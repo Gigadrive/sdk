a5e506d4da813d00136eb704b84335ceba7dd7f6
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2024-08-16 20:53:57 +0200

    chore: restructure project

---
+++ packages/cli/test-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bun run --cwd="$HOME/workspace/network/nebula/packages/elysia-example" $(pwd)/src/index.ts "$@"
--- test-run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bun run --cwd="$HOME/workspace/network/nebula/packages/elysia-example" $(pwd)/src/index.ts "$@"
