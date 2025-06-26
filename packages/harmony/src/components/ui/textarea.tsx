d86a65fe5c56342e7d14c2d822d5cde06cccc341
Mehdi Baaboura <mehdi.baaboura@gigadrive.de>
2025-06-26 02:39:17 +0200

    fix textarea ring color

--- packages/harmony/src/components/ui/textarea.tsx
+++ packages/harmony/src/components/ui/textarea.tsx
@@ -20,7 +20,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           <textarea
             id={id}
             className={cn(
-              'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+              'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:focus-visible:ring-offset-background',
               label !== undefined && label !== '' && 'pt-4',
               error !== undefined &&
                 'border-destructive focus:border-destructive dark:border-destructive dark:focus:border-destructive',
