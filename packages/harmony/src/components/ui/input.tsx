f1e488ada2d663fb049e23e7ba39ccc699101b23
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2025-09-29 21:51:37 +0200

    fix(harmony): adjust input component styles and update story examples

--- packages/harmony/src/components/ui/input.tsx
+++ packages/harmony/src/components/ui/input.tsx
@@ -30,7 +30,6 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
               'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:focus-visible:ring-offset-background',
               leftElement !== undefined && 'pl-9',
               rightElement !== undefined && 'pr-9',
-              label !== undefined && label !== '' && 'pt-4',
               error !== undefined &&
                 'border-destructive focus:border-destructive dark:border-destructive dark:focus:border-destructive',
               className
@@ -44,7 +43,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <label
               htmlFor={id}
               className={cn(
-                'pointer-events-none absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-stone-950',
+                'pointer-events-none absolute left-1.5 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-stone-950',
                 error !== undefined
                   ? 'text-destructive peer-focus:text-destructive dark:text-destructive peer-focus:dark:text-destructive'
                   : 'text-gray-500 peer-focus:text-primary-600 dark:text-gray-400 peer-focus:dark:text-primary-500',
